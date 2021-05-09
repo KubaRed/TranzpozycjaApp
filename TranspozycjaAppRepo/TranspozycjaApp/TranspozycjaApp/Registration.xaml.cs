30033f640c3afbbf925d77ebe6f190b641251044
Kuba <kuba.piecuch.kp@gmail.com>
2021-05-10 00:21:33 +0200

    dodanie kodu umorzliwiajacego zapisanie uzytkowanika przy rejestracji do bazy danych.

--- TranspozycjaAppRepo/TranspozycjaApp/TranspozycjaApp/Registration.xaml.cs
+++ TranspozycjaAppRepo/TranspozycjaApp/TranspozycjaApp/Registration.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TranspozycjaApp.Klasy;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace TranspozycjaApp
 {
@@ -22,6 +26,55 @@ namespace TranspozycjaApp
         public Registration()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string UserID = "";
+            string login = LoginText.Text;
+            string password = PasswordBoxText.Password;
+
+            AzureSQLConnection.openConnection();
+            AzureSQLConnection.sql = "select top 1 * from Users where Login='" + LoginText.Text + "'";
+            AzureSQLConnection.cmd.CommandType = CommandType.Text;
+            AzureSQLConnection.cmd.CommandText = AzureSQLConnection.sql;
+            AzureSQLConnection.da = new SqlDataAdapter(AzureSQLConnection.cmd);
+            AzureSQLConnection.dt = new DataTable();
+            AzureSQLConnection.da.Fill(AzureSQLConnection.dt);
+
+            try//dodawanie uzytkownika do bazy
+            {
+                DateTime dateTime = DateTime.Now;
+                string sqlFormattedDate = dateTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
+                AzureSQLConnection.openConnection();
+                AzureSQLConnection.sql = "INSERT INTO Users (Login, Password, Email) VALUES ('" + LoginText.Text + "', '" + PasswordBoxText.Password + "', '" + EmailText.Text + "')";
+                AzureSQLConnection.cmd.CommandType = CommandType.Text;
+                AzureSQLConnection.cmd.CommandText = AzureSQLConnection.sql;
+                AzureSQLConnection.cmd.ExecuteNonQuery(); //to wykonuje inserta :P
+                AzureSQLConnection.closeConnection();
+                //Insert do wagi, która zawiera informację o aktualnej dacie
+
+                //Pobranie ID dopiero co dodanego Usera
+                //AzureSQLConnection.openConnection();
+                //AzureSQLConnection.sql = "SELECT top 1 ID_User FROM Users WHERE Login='" + Login + "' AND Password='" + password + "'";
+                //AzureSQLConnection.cmd.CommandText = AzureSQLConnection.sql;
+                //AzureSQLConnection.rd = AzureSQLConnection.cmd.ExecuteReader();
+                //if (AzureSQLConnection.rd.Read())
+                //{
+                //    UserID = AzureSQLConnection.rd["ID_User"].ToString();
+                //    Console.WriteLine(UserID); //Do testowania
+                //}
+                //AzureSQLConnection.closeConnection(); 
+                                          
+         
+                MessageBox.Show("Użytkownik " + LoginText.Text + " został poprawnie utworzony.", "Rejestracja", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)//jesli baza nie dziala
+            {
+                MessageBox.Show("Błąd przy rejestracji"
+                   + Environment.NewLine + "opis: " + ex.Message.ToString(), "Rejestracja"
+                   , MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
